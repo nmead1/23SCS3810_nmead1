13dcc7c1bca2737f1fa43d38fa8d3340a7e62b64
nmead1 <nmead1@msudenver.edu>
2023-04-06 09:33:27 -0600

    added virtualenv files

---
+++ activity_14_psycopg/build/testHoteldb.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+params = {
+    'host': 'localhost', 
+    'port': 5432, 
+    'dbname': 'hotels', 
+    'user': 'hotel_manager',
+    'password': '135791'
+}
+
+conn = psycopg2.connect(**params)
+if conn: 
+    print('Connection to Postgres database ' + params['dbname'] + ' was successful!')
+
+    print('Bye!')
+    cur = conn.cursor()
+    sql = "INSERT INTO hotels (name, city, state) VALUES ('Broadmoor', 'Colorado Springs', 'CO');"
+    cur.execute(sql)
+    conn.commit()
+    sql = 'SELECT * FROM hotels'
+    cur.execute(sql)
+    for row in cur.fetchall(): 
+        print(row)
+    conn.close()
